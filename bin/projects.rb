a5d4fcee66747f755cbcacf01d2faf1f7640ce33
spikesolution <info@spikesolution.com>
2021-10-22 15:12:43 +0900

    Add script to fetch project data

---
+++ bin/projects.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require "json"
+require "uri"
+require "net/http"
+
+require_relative "../lib/graphql_client"
+
+client = GraphQlClient.new
+
+body = """
+{
+  projects(orderBy: timestamp) {
+    id
+    projectId
+  }
+}
+"""
+
+API_URL = "https://api.thegraph.com/subgraphs/name/co2ken/tokenizer"
+
+json = client.query(body: body, url: API_URL)
+
+rtn = JSON.parse(json)
+
+pp rtn
